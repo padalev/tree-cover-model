954f23fbf4817d4c45032944614bbd72969e4e01
Ruben Nitsche <mail@rubennitsche.eu>
2017-11-15 19:22:40 +0100

    Mal was geplottet

--- tree-cover-model.py
+++ tree-cover-model.py
@@ -1,8 +1,26 @@
-import matplotlib
-import numpy
-import scipy
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.integrate import ode
 
-print('hello tree cover')
+h_A = 10
+h_f = 64
+h_P = 0.5
+K = 90
+m_A = 0.15
+m_f = 0.11
+p = 7
+b = 0
+r_m = 0.3
+r_P = 1
 
-print('hello tree cover')
+def r(P):
+    return P/(h_P + P)*r_m
+def dTdt(P,T):
+    return P/(h_P + P)*r_m*T*(1-T/K)-m_A*T*h_A/(T + h_A) - m_f*T*h_f**p/(h_f**p + T**p)
+def dPdt(P,T):
+    return r_P*((P + b*T/K) - P)
 
+T = np.linspace(-1,90,1000)
+plt.plot(T,dTdt(3.5,T))
+plt.grid()
+plt.show()
