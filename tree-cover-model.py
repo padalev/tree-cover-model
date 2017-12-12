7ff8c1ae114fde2a7ed9b086fd14a0edec53fb01
Ruben Nitsche <mail@rubennitsche.eu>
2017-12-12 16:30:59 +0100

    Jetzt mit Newton-Suche und getrennter Sortierung etc.

--- tree-cover-model.py
+++ tree-cover-model.py
@@ -22,15 +22,14 @@ def dPdt(P,T):
     return r_P*((P + b*T/K) - P)
 
 
-plt.figure(1)
-T = np.linspace(1,100,1000)
-plt.plot(T,dTdt(T,1.5))
-#plt.plot(T,dTdt(1.5,T)**2)
-plt.grid()
+#plt.figure(1)
+#T = np.linspace(1,100,1000)
+#plt.plot(T,dTdt(T,1.5))
+#plt.grid()
 
 
 maxNumSolutions = 5
-precipitationvalues = 10000
+precipitationvalues = 100000
 Tvalues = 1000
 solutionsPstable = []
 solutionsTstable = []
@@ -40,56 +39,86 @@ solutionsTunstable = []
 precipitation = np.linspace(0,5,precipitationvalues)
 
 def zeroSolutions(pre):
-    zeropointsTstable = []
-    zeropointsPstable = []
-    zeropointsTunstable = []
-    zeropointsPunstable = []
+    def dTdtrun(x):
+        P = pre
+        return dTdt(x,P)
+    zeropointsT = []
+    zeropointsP = []
     T = np.linspace(-1,101,Tvalues)
     dt_ = dTdt(T,pre)
     dt = dt_**2
     for k in range(Tvalues-2):
         if ( dt[k+1] < dt[k] ):
             if ( dt[k+1] < dt[k+2] ):
-                if ( dt_[k] < 0 and dt_[k+2] > 0 ):
-                    zeropointsTunstable.append( T[k] )
-                    zeropointsPunstable.append( pre )
-                if ( dt_[k] > 0 and dt_[k+2] < 0 ):
-                    zeropointsTstable.append( T[k] )
-                    zeropointsPstable.append( pre )
-    return zeropointsPstable, zeropointsTstable, zeropointsPunstable, zeropointsTunstable
+                if ( dt_[k] < 0 and dt_[k+2] > 0 ) or ( dt_[k] > 0 and dt_[k+2] < 0 ):
+                    zeropointsT.append( optimize.newton( dTdtrun, T[k], maxiter=30 ) )
+                    zeropointsP.append( pre )
+    return zeropointsP, zeropointsT
 
-#Tvalues = 10
+def sortType(solutionsP, solutionsT):
+    zeropointsTstable = []
+    zeropointsPstable = []
+    zeropointsTunstable = []
+    zeropointsPunstable = []
+    for precounter, pre in enumerate(solutionsP):
+        if ( dTdt(solutionsT[precounter],pre) < 0 and dTdt(solutionsT[precounter]+0.1,pre) > 0 ):
+            zeropointsTunstable.append( solutionsT[precounter] )
+            zeropointsPunstable.append( pre )
+        if ( dTdt(solutionsT[precounter],pre) > 0 and dTdt(solutionsT[precounter]+0.1,pre) < 0 ):
+            zeropointsTstable.append( solutionsT[precounter] )
+            zeropointsPstable.append( pre )
+    zeropointsTstable,zeropointsPstable = zip(*sorted(zip(zeropointsTstable,zeropointsPstable)))
+    zeropointsTunstable,zeropointsPunstable = zip(*sorted(zip(zeropointsTunstable,zeropointsPunstable)))
+    return zeropointsTunstable, zeropointsPunstable, zeropointsTstable, zeropointsPstable
 
-#def zeroSolutionsNewton(pre):
-zeropointsTstable = []
-zeropointsPstable = []
-zeropointsTunstable = []
-zeropointsPunstable = []
-T = np.linspace(-1,101,Tvalues)
-def dTdtrun(x):
-    P = pre
-    return dTdt(x,P)
-#for k in range(Tvalues):
-plot(T,dTdtrun(T))
-print( optimize.newton( dTdtrun, 50, maxiter=50 ) )
-#zeropointsPstable.append( pre )
-return zeropointsPstable, zeropointsTstable, zeropointsPunstable, zeropointsTunstable
+def plotnsort(zeropointsTunstable, zeropointsPunstable, zeropointsTstable, zeropointsPstable):
+    tempP = []
+    tempT = []
+    for precounter, pre in enumerate(zeropointsPstable):
+        if (len(zeropointsPstable) > precounter+1):
+            if np.absolute(zeropointsTstable[precounter] - zeropointsTstable[precounter+1]) > 1:
+                tempP.append(pre)
+                tempT.append(zeropointsTstable[precounter])
+                tempP,tempT = zip(*sorted(zip(tempP,tempT)))
+                plt.plot(tempP,tempT,'k-')
+                tempP = []
+                tempT = []
+            else:
+                tempP.append(pre)
+                tempT.append(zeropointsTstable[precounter])
+    tempP,tempT = zip(*sorted(zip(tempP,tempT)))
+    plt.plot(tempP,tempT,'k-')
+    tempP = []
+    tempT = []
+    for precounter, pre in enumerate(zeropointsPunstable):
+        if (len(zeropointsPunstable) > precounter+1):
+            if np.absolute(zeropointsTunstable[precounter] - zeropointsTunstable[precounter+1]) > 1:
+                tempP.append(pre)
+                tempT.append(zeropointsTunstable[precounter])
+                tempP,tempT = zip(*sorted(zip(tempP,tempT)))
+                plt.plot(tempP,tempT,'k--')
+                tempP = []
+                tempT = []
+            else:
+                tempP.append(pre)
+                tempT.append(zeropointsTunstable[precounter])
+    tempP,tempT = zip(*sorted(zip(tempP,tempT)))
+    plt.plot(tempP,tempT,'k--')
 
+
+solutionsP = []
+solutionsT = []
 for precounter, pre in enumerate(precipitation):
     print(pre)
-    zeropointsPstable, zeropointsTstable, zeropointsPunstable, zeropointsTunstable = zeroSolutionsNewton(pre)
-    solutionsPstable.extend(zeropointsPstable)
-    solutionsTstable.extend(zeropointsTstable)
-    solutionsPunstable.extend(zeropointsPunstable)
-    solutionsTunstable.extend(zeropointsTunstable)
+    zeropointsP, zeropointsT = zeroSolutions(pre)
+    solutionsP.extend(zeropointsP)
+    solutionsT.extend(zeropointsT)
 
-
+solutionsTunstable, solutionsPunstable, solutionsTstable, solutionsPstable = sortType(solutionsP, solutionsT)
 #print "Die Antwort lautet: " + str(solutions)
-#solutionsTstable,solutionsPstable = zip(*sorted(zip(solutionsTstable,solutionsPstable)))
-#solutionsTunstable,solutionsPunstable = zip(*sorted(zip(solutionsTunstable,solutionsPunstable)))
-#plt.figure(2)
-#plt.plot(solutionsPstable,solutionsTstable,'k-')
-#plt.plot(solutionsPunstable,solutionsTunstable,'k--')
+#solutionsT,solutionsP = zip(*sorted(zip(solutionsT,solutionsP)))
+plt.figure(2)
+plotnsort(solutionsTunstable, solutionsPunstable, solutionsTstable, solutionsPstable)
 plt.grid()
 
 plt.show()
